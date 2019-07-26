2acc6b846e9e7c0be1a6523519b70e4b50449254
Carlos Mañas <carlos@spacenomads.com>
2019-07-26 10:23:02 +0200

    add zeros

--- js/main.js
+++ js/main.js
@@ -3,8 +3,22 @@
 const text = document.querySelector('.js__text');
 const container = document.querySelector('.js__container');
 
+
+
 for (let i=1;i<=100;i++) {
-  container.innerHTML += `<button class="container__button js__button">Botón nº ${i}</button>`;
+
+  let newI;
+  if (i === 100) {
+    newI = i;
+  } else if (i < 100 && i >=10 ){
+    newI = '0' + i;
+  } else {
+    newI = '00' + i;
+  }
+  console.log(i, newI);
+
+
+  container.innerHTML += `<button class="container__button js__button">Botón nº ${newI}</button>`;
 }
 
 const buttons = document.querySelectorAll('.js__button');
@@ -13,6 +27,7 @@ console.log(`Hay ${buttons.length} botones`);
 
 function clickMe(event) {
   text.innerHTML = event.currentTarget.innerHTML;
+
 }
 
 
